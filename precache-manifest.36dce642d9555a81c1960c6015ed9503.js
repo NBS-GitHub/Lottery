7ea7f23bd9f2a7938ff3c399557909ba89fd1d0f
pawelNobis <pawel.nobis1@gmail.com>
2019-02-13 20:04:14 +0100

    Updates

---
+++ precache-manifest.36dce642d9555a81c1960c6015ed9503.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b70048d2ff7ae5fd2c5dfc828a0e77bb",
+    "url": "/Lottery/static/media/debut_light.b70048d2.png"
+  },
+  {
+    "revision": "faaa30ce1d73a5cd8586",
+    "url": "/Lottery/static/js/runtime~main.faaa30ce.js"
+  },
+  {
+    "revision": "3836037addb119080b84",
+    "url": "/Lottery/static/js/main.3836037a.chunk.js"
+  },
+  {
+    "revision": "a852b0ebc3c774f96352",
+    "url": "/Lottery/static/js/1.a852b0eb.chunk.js"
+  },
+  {
+    "revision": "3836037addb119080b84",
+    "url": "/Lottery/static/css/main.77c280c1.chunk.css"
+  },
+  {
+    "revision": "b087f6bfa945d54179c6f9440e579190",
+    "url": "/Lottery/index.html"
+  }
+];
